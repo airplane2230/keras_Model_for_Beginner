f4955f1d76f1d9433364c6a9a7c7a64cfe6a1018
airplane2230 <miseonghwi@gmail.com>
2021-08-27 15:52:39 +0900

    Add Train Step

---
+++ simply_utils_example/tensorflow_train_raw/lr_schedule.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+class LRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(init_lr, warmup_epoch,
+                 decay_fn, init_epoch = 0):
+        self.init_lr = init_lr
+        self.decay_fn = decay_fn
+        self.warup_epoch = warmup_epoch
+        self.init_epoch = init_epoch
+        self.lr = 1e-4
+        
+    def get_config(self):
+        # not working
+        config = {
+            'learning_rate': self.lr
+        }
+        
+        return config
+    
+    # No Override
+    def on_epoch_begin(self, epoch, logs = None):
+        if self.init_epoch > 0:
+            epoch += self.init_epoch
+            
+        global_epoch = tf.cast(epoch + 1, tf.float64)
+        warmup_epoch_float = tf.cast(self.warmup_epoch, tf.float64)
+        
+        lr = tf.cond(
+            global_epoch < warmup_epoch_float,
+            lambda: tf.cast(self.init_lr * (global_epoch / warmup_epoch_float), tf.float64),
+            lambda: tf.cast(self.decay_fn(epoch - warmup_epoch_float), tf.float64)
+        )
+        
+        self.lr = lr
+        
+    def __call__(self, step):
+        self.on_epoch_begin(step, logs = None)
+        
+        return self.lr
---
+++ simply_utils_example/tensorflow_train_raw/train_raw.py
@@ -0,0 +1,144 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.models import Model
+import tensorflow as tf
+
+from lr_schedule import LRSchedule
+
+print(tf.__version__)
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+def make_datasets(x, y):
+    # (28, 28) -> (28, 28, 1)
+    def _new_axis(x, y):
+        y = tf.one_hot(y, depth = 10)
+        
+        return x[..., tf.newaxis], y
+            
+    ds = tf.data.Dataset.from_tensor_slices((x, y))
+    ds = ds.map(_new_axis, num_parallel_calls = tf.data.experimental.AUTOTUNE)
+    ds = ds.shuffle(100).batch(32) # 배치 크기 조절하세요
+    ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
+    
+    return ds
+    
+train_ds = make_datasets(x_train, y_train)
+
+# rescaling, 1 / 255
+preprocessing_layer = tf.keras.models.Sequential([
+        tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
+    ])
+
+# simple CNN model
+def get_model():
+    inputs = Input(shape = (28, 28, 1))
+    preprocessing_inputs = preprocessing_layer(inputs)
+    
+    x = Conv2D(filters = 32, kernel_size = (3, 3), activation='relu')(preprocessing_inputs)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(filters = 64, kernel_size = (3, 3), activation='relu')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(filters = 64, kernel_size =(3, 3), activation='relu')(x)
+    
+    x = Flatten()(x)
+    x = Dense(64, activation = 'relu')(x)
+    outputs = Dense(10, activation = 'softmax')(x)
+    
+    model = Model(inputs = inputs, outputs = outputs)
+    
+    return model
+
+model = get_model()
+# model.summary()
+
+# hyperparameter
+init_epoch = 1 # check
+EPOCHS = 1000 - (init_epoch - 1)
+warmup_epoch = int(EPOCHS * 0.3)
+init_lr = 0.1
+min_lr = 1e-6
+power = 1.
+
+lr_scheduler = tf.keras.optimizers.schedules.PolynomialDecay(
+    initial_learning_rate = init_lr,
+    decay_steps = EPOCHS - warmup_epoch,
+    end_learning_rate = min_lr,
+    power = power
+)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate = LRSchedule(init_lr,
+                                                                warmup_epoch=warmup_epoch,
+                                                                decay_fn=lr_scheduler,
+                                                                init_epoch=init_epoch))
+
+init_path = './model/model_ckpt/init'
+init_ckpt = tf.train.Checkpoint(model = model, optimizer = optimizer)
+
+init_ckpt_manager = tf.train.CheckpointManager(init_ckpt, init_path, max_to_keep = 10)
+init_ckpt_manager.save()
+print('save init!')
+
+@tf.function
+def train_step(inp, tar, training = True):
+    with tf.GradientTape() as tape:
+        predictions = model(inp)
+        loss = loss_function(tar, predictions)
+        
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_accuracy.update_state(tar, predictions)
+    
+    return loss, predictions
+
+# Train! 
+loss_plot = []
+loss_function = tf.keras.losses.CategoricalCrossentropy()
+
+train_accuracy = tf.keras.metrics.CategoricalAccuracy()
+
+ckpt_path = './model/model_ckpt/'
+ckpt = tf.train.Checkpoint(epoch = tf.Variable(1), loss = tf.Variable(1., dtype = tf.float64),
+                           accuracy = tf.Variable(1., dtype = tf.float64),
+                           model = model,
+                           optimizer = optimizer
+                           )
+ckpt_manager = tf.train.CheckpointManager(ckpt, ckpt_path, max_to_keep = 10)
+
+ckpt.restore(ckpt_manager.latest_checkpoint)
+if ckpt_manager.latest_checkpoint:
+    print(f'Restored from {ckpt_manager.latest_checkpoint}')
+else:
+    print('Initializing from Scratch')
+    
+for epoch in range(EPOCHS):
+    total_loss = 0.
+    train_accuracy.reset_states()
+    
+    tqdm_dataset = tqdm(enumerate(train_ds))
+    
+    LR_for_print = LRSchedule(init_lr,warmup_epoch=warmup_epoch,decay_fn=lr_scheduler,init_epoch=init_epoch)
+    
+    print('current learning_rate: ', LR_for_print(epoch + 1).numpy())
+    
+    for (batch, (tensor, target)) in tqdm_dataset:
+        batch_loss, predictions = train_step(tensor, target, training = True)
+        total_loss += batch_loss
+        
+        total_loss_format = total_loss / (batch + 1)
+        
+        tqdm_dataset.set_postfix({
+            'Epoch': epoch + 1,
+            'Loss': '{:06f}'.format(batch_loss.numpy()),
+            'Total Loss': '{:06f}'.format(total_loss_format),
+            'Accuracy': '{:04f}'.format(train-accuracy.result().numpy())
+        })
+        
+    loss_plot.append(total_loss_format)
+    
+    if np.min(loss_plot) == loss_plot[-1]:
+        print(f'{epoch + 1} - min Loss!: {loss_plot[-1]}, and Accuracy: {train_accuracy.result().numpy()}')
+        ckpt.epoch.assign(epoch + 1)
+        ckpt.accuracy.assign(train_accuracy.result().numpy())
+        ckpt.loss.assign(total_loss_format)
+        ckpt_manager.save()
